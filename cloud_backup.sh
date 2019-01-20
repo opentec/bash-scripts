86d4df7930947cbc287d4e374a845624ea95c983
opentec <ssommers22@yahoo.com>
2019-01-20 17:47:55 -0500

    Changed backup directory path to a variable

--- cloud_backup.sh
+++ cloud_backup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #This script is used for backing up Owncloud v10.0.1 database, config files and data files.
 
+#Target backup directory
+_dir="/media/pi"
+
 echo "Preparing to backup database";
-mysqldump --lock-tables -h localhost -u root -p owncloud > /media/pi/USB30FD/oc-dbbackups/owncloud-dbbackup_`date +"%Y%m%d"`.bak
+mysqldump --lock-tables -h localhost -u root -p owncloud > "$_dir"/oc-dbbackups/owncloud-dbbackup_`date +"%Y%m%d"`.bak
 if [ $? -ne 0 ]; then
 echo " "
 else
 echo "Finished backing up database";
 
 echo "Backing up config files";
-sudo cp -r /var/www/owncloud/config /media/pi/USB30FD/oc-dirbackups/
-mv /media/pi/USB30FD/oc-dirbackups/config /media/pi/USB30FD/oc-dirbackups/config_`date +"%Y%m%d"`
+sudo cp -r /var/www/owncloud/config "$_dir"/oc-dirbackups/
+mv "$_dir"/oc-dirbackups/config "$_dir"/oc-dirbackups/config_`date +"%Y%m%d"`
 echo "Finished backing up config files";
 
 echo "Backing up data files";
-sudo cp -r /media/ownclouddrive/ /media/pi/USB30FD/oc-filebackups/ownclouddrive_`date +"%Y%m%d"`
+sudo cp -r /media/ownclouddrive/ "$_dir"/oc-filebackups/ownclouddrive_`date +"%Y%m%d"`
 echo "Finished backing up data files";
 fi
