b4f99b71830a1dbb2c8e84ce73a1b73f07e7054a
opentec <ssommers22@yahoo.com>
2019-08-25 13:19:48 -0400

    Updated cp statements to rsync statements

--- cloud_backup.sh
+++ cloud_backup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#This script is used for backing up Owncloud v10.0.1 database, config files and data files.
+#This script is used for a full back up of Nextcloud database, config files and data files.
 
 #Target backup directory
-_dir="/media/pi"
+_dir="/media/usb"
 
 echo "Preparing to backup database";
-mysqldump --lock-tables -h localhost -u root -p owncloud > "$_dir"/oc-dbbackups/owncloud-dbbackup_`date +"%Y%m%d"`.bak
+sudo mysqldump --lock-tables -h localhost -u root -p nextcloud > "$_dir"/nc-dbbackups/nextcloud-dbbackup_`date +"%Y%m%d"`.bak
 if [ $? -ne 0 ]; then
 echo " "
 else
 echo "Finished backing up database";
 
 echo "Backing up config files";
-sudo cp -r /var/www/owncloud/config "$_dir"/oc-dirbackups/
-mv "$_dir"/oc-dirbackups/config "$_dir"/oc-dirbackups/config_`date +"%Y%m%d"`
+sudo rsync -av --progress /var/www/html/nextcloud/config "$_dir"/nc-dirbackups/
+mv "$_dir"/nc-dirbackups/config "$_dir"/nc-dirbackups/config_`date +"%Y%m%d"`
 echo "Finished backing up config files";
 
 echo "Backing up data files";
-sudo cp -r /media/ownclouddrive/ "$_dir"/oc-filebackups/ownclouddrive_`date +"%Y%m%d"`
+sudo rsync -av --progress /media/nextcloud/ "$_dir"/nc-filebackups/nextcloud_`date +"%Y%m%d"` --exclude updater-*
 echo "Finished backing up data files";
 fi
