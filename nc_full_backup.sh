75cd27ca9ec1275d2fcccab901730baf5cb54670
opentec <ssommers22@yahoo.com>
2019-12-27 17:39:10 -0500

    Updated destination directory and removed meta characters

--- nc_full_backup.sh
+++ nc_full_backup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#This script is used for a full back up of Nextcloud database, config files and data files.
+#This script is used perform a Full Back Up of Nextcloud database, config files and data files.
 
 #Target backup directory
-_dir="/media/usb"
+_destdir="/media/usb"
 
-echo "Preparing to backup database";
-sudo mysqldump --lock-tables -h localhost -u root -p nextcloud > "$_dir"/nc-dbbackups/nextcloud-dbbackup_`date +"%Y%m%d"`.bak
+echo "Preparing to backup database"
+sudo mysqldump --lock-tables -h localhost -u root -p nextcloud > "$_destdir"/nc-dbbackups/nextcloud-dbbackup_`date +"%Y%m%d"`.bak
 if [ $? -ne 0 ]; then
 echo " "
 else
-echo "Finished backing up database";
+echo "Finished backing up database"
 
-echo "Backing up config files";
-sudo rsync -av --progress /var/www/html/nextcloud/config "$_dir"/nc-dirbackups/config_`date +"%Y%m%d"`
-echo "Finished backing up config files";
+echo "Backing up config files"
+sudo rsync -av --progress /var/www/html/nextcloud/config "$_destdir"/nc-configbackups/config_`date +"%Y%m%d"`
+echo "Finished backing up config files"
 
-echo "Backing up data files";
-sudo rsync -av --progress /media/nextcloud/ "$_dir"/nc-filebackups/nextcloud_`date +"%Y%m%d"` --exclude updater-*
-echo "Finished backing up data files";
+echo "Backing up data files"
+sudo rsync -av --progress /media/nextcloud/ "$_destdir"/nc-filebackups/nextcloud_`date +"%Y%m%d"` --exclude updater-*
+echo "Finished backing up data files"
 fi
